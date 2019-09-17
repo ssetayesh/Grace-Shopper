b7042335c292ca8bf42d3d0171f8e1d9338ad48c
Kimberly Zawacki <ximberlyzawacki@gmail.com>
2019-09-17 17:54:08 -0400

    saved unsaved changes

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -2,11 +2,9 @@ const User = require('./user')
 const Item = require('./item')
 const Cart = require('./cart')
 
+// ASSOCIATIONS
 User.hasOne(Cart)
-
-Cart.belongTo(Item)
-
-// Item.hasMany(Cart)
+Item.hasMany(Cart)
 
 module.exports = {
   User,
