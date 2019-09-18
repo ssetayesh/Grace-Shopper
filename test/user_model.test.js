acfa55008aab306d9e93ef97b1a1e99961b0c39e
Kimberly Zawacki <ximberlyzawacki@gmail.com>
2019-09-18 16:30:04 -0400

    test: created structure for test specs and added specs testing user validation

--- test/user_model.test.js
+++ test/user_model.test.js
@@ -1,29 +1,54 @@
-const Promise = require('bluebird')
-const chai = require('chai')
-const expect = chai.expect
-const sinon = require('sinon')
-const sinonChai = require('sinon-chai')
-chai.use(sinonChai)
+const {expect} = require('chai')
 
 const db = require('../server/db/db')
-const {User, Product, Order} = require('../server/db/models')
+const {User} = require('../server/db/models')
 
-describe('The User Model', () => {
-  before(() => {
+describe('User Model', () => {
+  beforeEach(() => {
     return db.sync({force: true})
   })
 
   let user
-  beforeEach(() => {
-    user = User.build({
+  beforeEach(async () => {
+    user = await User.create({
       email: 'helloworld@email.com',
       password: 'password',
       address: '123 Sesame Street'
     })
   })
 
-  it('user attributes', async () => {
-    const savedUser = await user.save()
-    expect(savedUser.email).to.equal('')
+  describe('user attributes', async () => {
+    it('has email attribute', () => {
+      expect(user.email).to.equal('helloworld@email.com')
+    })
+
+    it('has hidden password attribute', () => {
+      expect(typeof user.password).to.equal('function')
+    })
+
+    it('requires `email`', async () => {
+      user.email = null
+      let emailIsNull, error
+
+      try {
+        emailIsNull = await user.validate()
+      } catch (err) {
+        error = err
+      }
+      if (emailIsNull) throw Error('validation should fail if email is null')
+
+      expect(error).to.be.an.instanceOf(Error)
+    })
+  })
+  describe('instanceMethods', () => {
+    describe('correctPassword', () => {
+      it('returns true if the password is correct', () => {
+        expect(user.correctPassword('password')).to.be.equal(true)
+      })
+
+      it('returns false if the password is incorrect', () => {
+        expect(user.correctPassword('pusword')).to.be.equal(false)
+      })
+    })
   })
 })
