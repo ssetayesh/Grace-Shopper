1c21f61af1aa78f46ccce6cbdc9ed02fe2573cb5
Kimberly Zawacki <ximberlyzawacki@gmail.com>
2019-09-18 14:52:43 -0400

    test: wrote tests for user attributes and created test folder

---
+++ test/user_model.test.js
@@ -0,0 +1,29 @@
+const Promise = require('bluebird')
+const chai = require('chai')
+const expect = chai.expect
+const sinon = require('sinon')
+const sinonChai = require('sinon-chai')
+chai.use(sinonChai)
+
+const db = require('../server/db/db')
+const {User, Product, Order} = require('../server/db/models')
+
+describe('The User Model', () => {
+  before(() => {
+    return db.sync({force: true})
+  })
+
+  let user
+  beforeEach(() => {
+    user = User.build({
+      email: 'helloworld@email.com',
+      password: 'password',
+      address: '123 Sesame Street'
+    })
+  })
+
+  describe('user attributes', async () => {
+    const savedUser = await user.save()
+    expect(savedUser.title).to.equal('')
+  })
+})
