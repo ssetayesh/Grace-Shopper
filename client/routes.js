53955017f3274b61c102993aa9245e38f24a1d22
Shirin Setayesh <shannonsetayesh@gmail.com>
2019-09-20 14:32:30 -0400

    style: adding Home route

--- client/routes.js
+++ client/routes.js
@@ -22,6 +22,7 @@ class Routes extends Component {
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
+        <Route exact path="/" component={Home} />
         <Route exact path="/items" component={AllItems} />
         <Route exact path="/guest/cart" component={Cart} />
         <Route path="/login" component={Login} />
