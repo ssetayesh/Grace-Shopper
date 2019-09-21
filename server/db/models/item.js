3fe83490c0d7779b23954ec715e06d03a41ba6f2
Gundeep Kohli <gkohli1@fordham.edu>
2019-09-20 20:03:31 -0400

    refactor: removed quantity from items table

--- server/db/models/item.js
+++ server/db/models/item.js
@@ -12,9 +12,6 @@ const Item = db.define('item', {
     type: Sequelize.STRING,
     defaultValue: 'https://image.flaticon.com/icons/svg/8/8109.svg'
   },
-  quantity: {
-    type: Sequelize.INTEGER
-  },
   price: {
     type: Sequelize.DECIMAL(10, 2)
   }
