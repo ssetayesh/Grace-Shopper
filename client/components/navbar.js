a43cf1139f6c584dcc54de7d28b21359080d727f
Shirin Setayesh <shannonsetayesh@gmail.com>
2019-09-24 23:40:31 -0400

    chore: navbar

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
